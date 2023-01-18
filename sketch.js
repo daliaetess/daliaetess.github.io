ee637d86e558a35480d3e51b4a0bcbf1bf3705e4
Dalia Etessami <daliaetess@gmail.com>
2023-01-18 15:40:25 -0500

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,45 @@
+function setup() {
+	createCanvas(800, 800);
+  }
+  
+function draw() {
+
+	background(247, 250, 245);
+
+	push();
+	fill(230, 24, 170, 100);
+	translate(400, 400);
+	noStroke();
+	rotate(radians(frameCount / -50));
+	for (var r1 = 0; r1 < hour(); r1++) {
+	  ellipse(10, 260, 85, 150);
+	  let d = hour()/2
+	  rotate(PI / d);
+	}
+	pop();
+
+	push();
+	fill(245, 180, 25, 125);
+	translate(400, 400);
+	noStroke();
+	rotate(radians(frameCount / 50));
+	for (var r1 = 0; r1 < minute(); r1++) {
+	  ellipse(-10, 145, 24, 100);
+	  let c = minute()/2
+	  rotate(PI / c);
+	}
+	pop();
+
+	push();
+	fill(75, 50, 45, 150);
+	translate(400, 400);
+	noStroke();
+	rotate(radians(frameCount / -75));
+	for (var r1 = 0; r1 < second(); r1++) {
+	  ellipse(-5, 70, 10, 20);
+	  let a = second()/2
+	  rotate(PI / a);
+	}
+	pop();
+
+  }
